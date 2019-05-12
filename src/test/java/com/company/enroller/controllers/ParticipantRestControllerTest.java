976ec84829af6dbebfe061029dcc819d19ba32da
student17 <student17@pcoip.ki.agh.edu.pl>
2019-05-12 10:36:04 +0200

    Participants test added

--- src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
+++ src/test/java/com/company/enroller/controllers/ParticipantRestControllerTest.java
@@ -5,8 +5,12 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import java.util.Collection;
 
@@ -19,9 +23,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
 import com.company.enroller.persistence.ParticipantService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ParticipantRestController.class)
@@ -47,6 +53,70 @@ public class ParticipantRestControllerTest {
 
 		mvc.perform(get("/participants").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].login", is(participant.getLogin())));
+	}
+
+	@Test
+	public void getParticipant() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+
+		given(participantService.findByLogin(participant.getLogin())).willReturn(participant);
+
+		mvc.perform(get("/participants/testlogin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(new ObjectMapper().writeValueAsString(participant)));
+	}
+	
+	@Test
+	public void addParticipant() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+
+		mvc.perform(post("/participants").contentType(MediaType.APPLICATION_JSON)
+		.content("{\"login\":\"testlogin\",\"password\":\"testpassword\"}"))
+		.andExpect(status().isCreated())
+		.andExpect(content().string(new ObjectMapper().writeValueAsString(participant)));
+	}
+	
+	@Test
+	public void addExisitingParticipants() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+
+		given(participantService.findByLogin("testlogin")).willReturn(participant);
+
+		mvc.perform(post("/participants").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"login\":\"testlogin\",\"password\":\"testpassword\"}"))
+				.andExpect(status().isConflict());
+	}
+	
+	@Test
+	public void deleteParticipant() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+
+		given(participantService.findByLogin("testlogin")).willReturn(participant);
+
+		mvc.perform(delete("/participants/testlogin"))
+				.andExpect(status().isNoContent());
+	}
+	
+	@Test
+	public void updateParticipant() throws Exception {
+		Participant participant = new Participant();
+		participant.setLogin("testlogin");
+		participant.setPassword("testpassword");
+
+		given(participantService.findByLogin("testlogin")).willReturn(participant);
+//		given(participantService.update(participant));
+		
+		mvc.perform(put("/participants/testlogin")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"login\":\"testlogin\",\"password\":\"testpassword222\"}"))
+				.andExpect(status().isNoContent());
 	}
 
 }
